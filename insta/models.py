b1133054d712cf579cdad06853f18c1e35f911ec
Nkurumwa <nkurumwa51@gmail.com>
2019-11-18 09:08:51 +0300

    adds profile class

--- insta/models.py
+++ insta/models.py
@@ -6,3 +6,38 @@ from django.utils import timezone
 from tinymce.models import HTMLField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(default='default.jpg', upload_to='profile_pics/')
+    bio = models.TextField(blank=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+        
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def search_profile(cls, name):
+        profile = Profile.objects.filter(user__username__icontains = name)
+        return profile
+
+    @classmethod
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(user = id)
+        return profile
+
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user = id).first()
+        return profile
