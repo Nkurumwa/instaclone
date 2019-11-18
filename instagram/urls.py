88f8270b1f7375acfa10416b9d003c072b39ca07
Nkurumwa <nkurumwa51@gmail.com>
2019-11-18 10:09:22 +0300

    updates project urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url,include
+from django.contrib.auth import views
 from django.contrib import admin
-from django.urls import path
+from clones import views as clones_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('clones.urls')),
+    url('login/', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    # url(r'^accounts/', include('registration.backends.simple.urls')),
+    url('^register/', clones_views.register, name='register'),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+    url(r'^tinymce/', include('tinymce.urls')),
 ]
