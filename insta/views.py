cf8121d8f11f1af0f57cb537018f7ffb2da83ef1
Nkurumwa <nkurumwa51@gmail.com>
2019-11-18 11:46:46 +0300

    modifies views

--- insta/views.py
+++ insta/views.py
@@ -12,11 +12,6 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            
-            # email = form.cleaned_data['email']
-            # recipient = NewsLetterRecipients(name = name,email =email)
-            # recipient.save()
-            # send_welcome_email(name,email)
 
             return redirect('/accounts/login/')
             
