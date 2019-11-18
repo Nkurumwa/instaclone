0329e67127d186f7f20a3e5710bbb4a6a76145f9
Nkurumwa <nkurumwa51@gmail.com>
2019-11-18 09:08:17 +0300

    imports all necessary files

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+from PIL import Image as pil_img
+import datetime
+from django.utils import timezone
+from tinymce.models import HTMLField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
