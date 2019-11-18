733c7d96cb334fdad0c6e58bd1b8479d8e92366d
Nkurumwa <nkurumwa51@gmail.com>
2019-11-18 09:59:59 +0300

    imports all required fields

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect
+from .models import Image, Profile,Comment
+from django.contrib.auth.models import User
+from .forms import CommentForm, ImageForm, ProfileUpdateForm, UserUpdateForm, PostIMageForm, UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .email import 
