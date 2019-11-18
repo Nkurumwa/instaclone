f164bf8e6710725c85871ec5c252023c81a55659
Nkurumwa <nkurumwa51@gmail.com>
2019-11-18 10:02:25 +0300

    implements single image function

--- insta/views.py
+++ insta/views.py
@@ -49,3 +49,21 @@ def profile(request, username):
     return render(request, 'profiles/profile.html', {'profile':profile, 'profile_details':profile_details, 'user_images':user_images})
 
 
+@login_required(login_url='/accounts/login')
+def single_image(request, image_id):
+    image = Image.get_image_id(image_id)
+    comments = Comment.get_comments_by_images(image_id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.image = image
+            comment.user = request.user
+            comment.save()
+            return redirect('single_image', image_id=image_id)
+    else:
+        form = CommentForm()
+        
+    return render(request, 'single_image.html', {'image':image, 'form':form, 'comments':comments})
+
